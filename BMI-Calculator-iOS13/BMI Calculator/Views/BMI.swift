38aa64a956f69f1c77fdbfd436ef6a71da93403d
Can ULUBAŞ <100785376+HCU13@users.noreply.github.com>
2022-05-19 14:56:12 +0300

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Views/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Can ULUBAŞ on 19.05.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+
+import UIKit
+
+struct BMI {
+    let value : Float
+    let advice : String
+    let color : UIColor
+    
+}
